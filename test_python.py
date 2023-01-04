0972506b79d94621e8139f24b4eaf17e78ce7b56
Alex-Vacool <vacoolenko@gmail.com>
2023-01-04 16:50:58 +0300

    Добавлены файлы тестов и проведено тестирование функций

--- test_python.py
+++ test_python.py
@@ -4,10 +4,29 @@
 
 import math as m
 
-assert m.pi == 3.141592653589793
-assert m.sqrt(9) == 3
-assert m.sqrt(25) == 5
-assert m.pow(2, 3) == 8
-assert m.pow(10, 2) == 100
-assert m.hypot(3, 4) == 5.0
-assert m.hypot(6, 6) == 8.48528137423857
+#########################################################
+numbers = [10, 15, 21, 33, 42, 55]
+#########################################################
+mapped_numbers = list(map(lambda x: x * 2 + 3, numbers))
+print(mapped_numbers)
+#########################################################
+sorted_numbers = sorted(numbers, reverse=True)
+print(sorted_numbers)
+#########################################################
+assert mapped_numbers == [23, 33, 45, 69, 87, 113]
+assert sorted_numbers == [55, 42, 33, 21, 15, 10]
+#########################################################
+def test_pi():
+    assert m.pi == 3.141592653589793
+def test_sqrt():
+    assert m.sqrt(9) == 3
+def test_sqrt2():
+    assert m.sqrt(25) == 5
+def test_pow():
+    assert m.pow(2, 3) == 8
+def test_pow2():
+    assert m.pow(10, 2) == 100
+def test_hypot():
+    assert m.hypot(3, 4) == 5.0
+def test_hypot2():
+    assert m.hypot(6, 6) == 8.48528137423857
